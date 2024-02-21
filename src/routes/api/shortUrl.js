75040e2e08d81979190099e6504dbb93a48a7ca7
.M.U.F.A.R. <132034817+Pkok1024@users.noreply.github.com>
2024-02-21 17:23:03 +0000

    up

--- src/routes/api/shortUrl.js
+++ src/routes/api/shortUrl.js
@@ -1,35 +1,45 @@
-import express from 'express';
-import apiKeyMiddleware from '../../middlewares/apiKeyMiddleware.js';
-import scrape from '../../scrape/index.js';
-import fetch from 'node-fetch';
+import express from 'express'
+import apiKeyMiddleware from '../../middlewares/apiKeyMiddleware.js'
+import scrape from '../../scrape/index.js'
+import fetch from 'node-fetch'
 
-const apiR = express.Router();
+const apiR = express.Router()
 
 apiR.get('/:shortener', apiKeyMiddleware, async (req, res) => {
   try {
-    const { url } = req.query;
+    const { url } = req.query
 
     if (!url) {
       return res.status(400).json({
         error: 'URL is required.',
-      });
+      })
     }
 
-    let shortUrl;
+    if (!url || !isValidURL(url)) {
+      return res.status(400).json({
+        error: 'Valid URL is required.',
+      })
+    }
+
+    let shortUrl
 
     switch (req.params.shortener) {
       case 'isgd':
-        shortUrl = await shortenWithIsGd(url);
-        break;
-        
+        shortUrl = await shortenWithIsGd(url)
+        break
+
       case 'tiny':
-        shortUrl = await scrape.shortlink(encodeURIComponent(url));
-        break;
+        shortUrl = await scrape.shortlink(decodeURIComponent(url))
+        break
+
+      case 'vurl':
+        shortUrl = await VURL(decodeURIComponent(url))
+        break
 
       default:
         return res.status(400).json({
           error: 'Invalid shortener specified.',
-        });
+        })
     }
 
     res.json({
@@ -37,22 +47,36 @@ apiR.get('/:shortener', apiKeyMiddleware, async (req, res) => {
       code: 200,
       author,
       data: shortUrl,
-    });
+    })
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal server error.' });
+    console.error(error)
+    res.status(500).json({ error: 'Internal server error.' })
   }
-});
+})
+
+async function VURL(url) {
+  const response = await fetch(`https://vurl.com/api.php?url=${url}`)
+  if (!response.ok) {
+    throw new Error(`HTTP error ! Status: &{response.status}`)
+  }
+  const json = await response.json()
+  return json
+}
 
 async function shortenWithIsGd(url) {
   const response = await fetch(
-    `https://is.gd/create.php?format=json&url=${encodeURIComponent(url)}`
-  );
+    `https://is.gd/create.php?format=json&url=${decodeURIComponent(url)}`
+  )
   if (!response.ok) {
-    throw new Error(`HTTP error! Status: ${response.status}`);
+    throw new Error(`HTTP error! Status: ${response.status}`)
   }
-  const json = await response.json();
-  return json.shorturl;
+  const json = await response.json()
+  return json.shorturl
 }
 
-export default apiR;
+function isValidURL(url) {
+  const urlRegex = /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/
+  return urlRegex.test(url)
+}
+
+export default apiR
