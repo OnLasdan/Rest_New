85386b3ac55b807e386652c702b72d5c39788526
M.U.F.A.R <132034817+Pkok1024@users.noreply.github.com>
2024-02-21 23:17:06 +0700

    Update shortUrl.js

--- src/routes/api/shortUrl.js
+++ src/routes/api/shortUrl.js
@@ -1,64 +1,54 @@
-import express from 'express'
-import apiKeyMiddleware from '../../middlewares/apiKeyMiddleware.js'
-import scrape from '../../scrape/index.js'
-import fetch from 'node-fetch'
-const apiR = express.Router()
+import express from 'express';
+import apiKeyMiddleware from '../../middlewares/apiKeyMiddleware.js';
+import scrape from '../../scrape/index.js';
+import fetch from 'node-fetch';
 
-apiR.get('/isgd', apiKeyMiddleware, async (req, res) => {
+const apiR = express.Router();
+
+apiR.get('/:shortener', apiKeyMiddleware, async (req, res) => {
   try {
-    const { url } = req.query
+    const { url } = req.query;
 
     if (!url) {
       return res.status(400).json({
-        error: 'url are required.',
-      })
+        error: 'URL is required.',
+      });
     }
-    const response = await fetch(
-      `https://is.gd/create.php?format=json&url=${encodeURIComponent(url)}`
-    )
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status:
-    ${response.status}`)
+
+    let shortUrl;
+
+    switch (req.params.shortener) {
+      case 'isgd':
+        const response = await fetch(
+          `https://is.gd/create.php?format=json&url=${encodeURIComponent(url)}`
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const json = await response.json();
+        shortUrl = json.shorturl;
+        break;
+
+      case 'tiny':
+        shortUrl = await scrape.shortlink(encodeURIComponent(url));
+        break;
+
+      default:
+        return res.status(400).json({
+          error: 'Invalid shortener specified.',
+        });
     }
-    const json = await response.json()
-    console.log(json)
-    const data = json.shorturl
 
     res.json({
       status: 'Success',
       code: 200,
       author,
-      data,
-    })
+      data: shortUrl,
+    });
   } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: 'Internal server error.' })
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error.' });
   }
-})
+});
 
-apiR.get('/tiny', apiKeyMiddleware, async (req, res) => {
-  try {
-    const { url } = req.query
-    if (!url) {
-      return res.status(400).json({
-        error: 'url parameter required.',
-      })
-    }
-
-    const data = await scrape.shortlink(encodeURIComponent(url))
-
-    res.json({
-      status: 'Success',
-      code: 200,
-      author,
-      data,
-    })
-  } catch (e) {
-    console.log(e)
-    res.status(500).json({
-      error: 'Internal Server Error ',
-    })
-  }
-})
-
-export default apiR
+export default apiR;
