aa6ae5159e20d893d56fb3ef3ff583bb6eda8c64
.M.U.F.A.R. <132034817+Pkok1024@users.noreply.github.com>
2024-02-23 10:50:58 +0000

    x

--- src/routes/api/upload.js
+++ src/routes/api/upload.js
@@ -10,18 +10,11 @@ const upload = multer({ storage })
 
 apiR.post('/cdn', upload.single('file'), async (req, res) => {
   try {
-    if (user.limit <= 0) {
-      return res.status(403).json({ error: 'Limit exceeded.' })
-    }
-    if (!req.file) return res.status(400).json({ error: 'No file uploaded.' })
-
+    
     const { buffer, originalname } = req.file
     const ext = path.extname(originalname)
     const result = await sendFile(buffer, ext)
 
-    user.limit -= 1
-    await user.save()
-
     res.json({
       status: 'Success',
       code: 200,
@@ -36,28 +29,5 @@ apiR.post('/cdn', upload.single('file'), async (req, res) => {
     })
   }
 })
-
-apiR.post('/upload', upload.single('file'), async (req, res) => {
-  try {
-    const { buffer, originalname } = req.file
-    const ext = path.extname(originalname)
-    const result = await pixeldrain(buffer)
-
-    res.json({
-      status: 'Success',
-      code: 200,
-      author: 'Xyla',
-      data: result,
-    })
-  } catch (error) {
-    console.error('Error processing file upload:', error)
-    res.status(500).json({
-      error: 'Internal Server Error',
-      message: 'An error occurred while processing the file upload.',
-    })
-  }
-})
-
-
 
 export default apiR
