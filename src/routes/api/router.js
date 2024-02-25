ba108e210f6a243ad12ecf098aad401e9389aac5
.M.U.F.A.R. <132034817+Pkok1024@users.noreply.github.com>
2024-02-25 21:08:57 +0000

    a

--- src/routes/api/router.js
+++ src/routes/api/router.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { fileURLToPath } from 'url'
 import { dirname } from 'path'
-
+import fs from 'fs'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
